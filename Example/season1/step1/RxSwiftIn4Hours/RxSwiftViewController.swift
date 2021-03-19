488d2fe9abf205509d8d5130c9a38a150c6eed3d
indoni <choinj97@gmail.com>
2021-03-20 00:00:11 +0900

    Update example files

---
+++ Example/season1/step1/RxSwiftIn4Hours/RxSwiftViewController.swift
@@ -0,0 +1,70 @@
+//
+//  RxSwiftViewController.swift
+//  RxSwiftIn4Hours
+//
+//  Created by iamchiwon on 21/12/2018.
+//  Copyright © 2018 n.code. All rights reserved.
+//
+
+import RxSwift
+import UIKit
+
+class RxSwiftViewController: UIViewController {
+    // MARK: - Field
+
+    var counter: Int = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+            self.counter += 1
+            self.countLabel.text = "\(self.counter)"
+        }
+    }
+
+    // MARK: - IBOutlet
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var countLabel: UILabel!
+
+    // MARK: - IBAction
+
+    var disposable: Disposable?
+    
+    @IBAction func onLoadImage(_ sender: Any) {
+        imageView.image = nil
+
+        disposable = rxswiftLoadImage(from: LARGER_IMAGE_URL)
+            .observeOn(MainScheduler.instance)
+            .subscribe({ result in
+                switch result {
+                case let .next(image):
+                    self.imageView.image = image
+
+                case let .error(err):
+                    print(err.localizedDescription)
+
+                case .completed:
+                    break
+                }
+            })
+    }
+
+    @IBAction func onCancel(_ sender: Any) {
+        // TODO: cancel image loading
+        disposable?.dispose()
+        // 생성 된 옵져버블이 동작하고 있는 동안(image loading)의 작업을 dispose 시킬 수 있다.
+    }
+
+    // MARK: - RxSwift
+
+    func rxswiftLoadImage(from imageUrl: String) -> Observable<UIImage?> {
+        return Observable.create { seal in
+            asyncLoadImage(from: imageUrl) { image in
+                seal.onNext(image)
+                seal.onCompleted()
+            }
+            return Disposables.create()
+        }
+    }
+}
