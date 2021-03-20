7bb61bdcff93af3ae41450ea00ef28c6d96677c4
indoni <choinj97@gmail.com>
2021-03-20 16:17:48 +0900

    Rxcocoa test in step3

--- Example/season1/step3/RxSwiftIn4Hours/ViewController.swift
+++ Example/season1/step3/RxSwiftIn4Hours/ViewController.swift
@@ -34,6 +34,50 @@ class ViewController: UIViewController {
         //          +--> button enable
         //          |
         // pw input +--> check valid --> bullet
+        
+        // MARK: - 방법 1. 기본적인 .rx를 이용한 UI test
+        idField.rx.text.orEmpty // 아래 nil 검증해주는 애 2줄 == .orEmpty
+//            .filter { $0 != nil } // string이 옵셔널이니까 nil인지 체크
+//            .map { $0! } // 강제 unwrapping
+            .map(checkEmailValid)
+            .subscribe(onNext: { b in
+                self.idValidView.isHidden = b
+            })
+            .disposed(by: disposeBag)
+        
+        pwField.rx.text.orEmpty
+            .map(checkPasswordValid)
+            .subscribe(onNext: { b in
+                self.pwValidView.isHidden = b
+            })
+            .disposed(by: disposeBag)
+        
+        Observable.combineLatest(
+            idField.rx.text.orEmpty.map(checkEmailValid),
+            pwField.rx.text.orEmpty.map(checkPasswordValid),
+            resultSelector: { s1, s2 in s1 && s2 }
+        )
+            .subscribe(onNext: { b in
+                self.loginButton.isEnabled = b
+            })
+            .disposed(by: disposeBag)
+        
+        // MARK: - 방법 2. .rx를 더 고급스럽게 사용해서 UI test
+        // input : id value, pw value
+//        let idInputOb: Observable<String> = idField.rx.text.orEmpty.asObservable()
+//        let idValidOb = idInputOb.map(checkEmailValid)
+//        
+//        let pwInputOb: Observable<String> = pwField.rx.text.orEmpty.asObservable()
+//        let pwValidOb = pwInputOb.map(checkPasswordValid)
+//        
+//        // output : red bullet button, login button
+//        idValidOb.subscribe(onNext: { b in self.idValidView.isHidden = b })
+//            .disposed(by: disposeBag)
+//        pwValidOb.subscribe(onNext: { b in self.pwValidView.isHidden = b })
+//            .disposed(by: disposeBag)
+//        Observable.combineLatest(idValidOb, pwValidOb, resultSelector: { $0 && $1 })
+//            .subscribe(onNext: { b in self.loginButton.isEnabled = b })
+//            .disposed(by: disposeBag)
     }
 
     // MARK: - Logic
