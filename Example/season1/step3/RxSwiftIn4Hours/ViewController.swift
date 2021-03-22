b14d49a7eeb1ac4a9a1df7363aa520dc0ee9fdd4
indoni <choinj97@gmail.com>
2021-03-22 23:41:06 +0900

    Subject 실습

--- Example/season1/step3/RxSwiftIn4Hours/ViewController.swift
+++ Example/season1/step3/RxSwiftIn4Hours/ViewController.swift
@@ -13,9 +13,16 @@ import UIKit
 class ViewController: UIViewController {
     var disposeBag = DisposeBag()
 
+    let idValid: BehaviorSubject<Bool> = BehaviorSubject(value: false)
+    let pwValid: BehaviorSubject<Bool> = BehaviorSubject(value: false)
+    
+    let idInputText: BehaviorSubject<String> = BehaviorSubject(value: "")
+    let pwInputText: BehaviorSubject<String> = BehaviorSubject(value: "")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        bindUI()
+        bindInput()
+        bindOutput()
     }
 
     // MARK: - IBOutler
@@ -28,7 +35,7 @@ class ViewController: UIViewController {
 
     // MARK: - Bind UI
 
-    private func bindUI() {
+    private func bindInput() {
         // id input +--> check valid --> bullet
         //          |
         //          +--> button enable
@@ -36,48 +43,77 @@ class ViewController: UIViewController {
         // pw input +--> check valid --> bullet
         
         // MARK: - 방법 1. 기본적인 .rx를 이용한 UI test
-        idField.rx.text.orEmpty // 아래 nil 검증해주는 애 2줄 == .orEmpty
+//        idField.rx.text.orEmpty // 아래 nil 검증해주는 애 2줄 == .orEmpty
 //            .filter { $0 != nil } // string이 옵셔널이니까 nil인지 체크
 //            .map { $0! } // 강제 unwrapping
-            .map(checkEmailValid)
-            .subscribe(onNext: { b in
-                self.idValidView.isHidden = b
-            })
-            .disposed(by: disposeBag)
-        
-        pwField.rx.text.orEmpty
-            .map(checkPasswordValid)
-            .subscribe(onNext: { b in
-                self.pwValidView.isHidden = b
-            })
-            .disposed(by: disposeBag)
-        
-        Observable.combineLatest(
-            idField.rx.text.orEmpty.map(checkEmailValid),
-            pwField.rx.text.orEmpty.map(checkPasswordValid),
-            resultSelector: { s1, s2 in s1 && s2 }
-        )
-            .subscribe(onNext: { b in
-                self.loginButton.isEnabled = b
-            })
-            .disposed(by: disposeBag)
+//            .map(checkEmailValid)
+//            .subscribe(onNext: { b in
+//                self.idValidView.isHidden = b
+//            })
+//            .disposed(by: disposeBag)
+//
+//        pwField.rx.text.orEmpty
+//            .map(checkPasswordValid)
+//            .subscribe(onNext: { b in
+//                self.pwValidView.isHidden = b
+//            })
+//            .disposed(by: disposeBag)
+//
+//        Observable.combineLatest(
+//            idField.rx.text.orEmpty.map(checkEmailValid),
+//            pwField.rx.text.orEmpty.map(checkPasswordValid),
+//            resultSelector: { s1, s2 in s1 && s2 }
+//        )
+//            .subscribe(onNext: { b in
+//                self.loginButton.isEnabled = b
+//            })
+//            .disposed(by: disposeBag)
         
         // MARK: - 방법 2. .rx를 더 고급스럽게 사용해서 UI test
         // input : id value, pw value
-//        let idInputOb: Observable<String> = idField.rx.text.orEmpty.asObservable()
-//        let idValidOb = idInputOb.map(checkEmailValid)
-//        
-//        let pwInputOb: Observable<String> = pwField.rx.text.orEmpty.asObservable()
-//        let pwValidOb = pwInputOb.map(checkPasswordValid)
-//        
-//        // output : red bullet button, login button
-//        idValidOb.subscribe(onNext: { b in self.idValidView.isHidden = b })
-//            .disposed(by: disposeBag)
-//        pwValidOb.subscribe(onNext: { b in self.pwValidView.isHidden = b })
-//            .disposed(by: disposeBag)
-//        Observable.combineLatest(idValidOb, pwValidOb, resultSelector: { $0 && $1 })
-//            .subscribe(onNext: { b in self.loginButton.isEnabled = b })
-//            .disposed(by: disposeBag)
+        //let idInputOb: Observable<String> = idField.rx.text.orEmpty.asObservable()
+        
+        // MARK - final start
+        // id필드 내용 저장
+        idField.rx.text.orEmpty
+            .bind(to: idInputText)
+            .disposed(by: disposeBag)
+        // id필드 valid check
+        idInputText
+            .map(checkEmailValid)
+            .bind(to: idValid)
+            .disposed(by: disposeBag)
+        // bind return 값이 Disposable이므로 .disposed 해줌
+        
+        // BehaviorSubject에 값 넣어주기! (onNext 사용해서)
+        // idValidOb.subscribe(onNext: { b in
+        //    self.idValid.onNext(b)
+        // })
+        // 혹은 아래 방법! (이걸 더 자주씀, 위윗줄에 map 뒤에 바로 붙혀서 간결하게 사용)
+        // idValidOb.bind(to: idValid)
+        
+        // let pwInputOb: Observable<String> = pwField.rx.text.orEmpty.asObservable()
+        
+        // id필드 내용 저장
+        pwField.rx.text.orEmpty
+            .bind(to: pwInputText)
+            .disposed(by: disposeBag)
+        
+        // pw 필드 valid check
+        pwInputText
+            .map(checkPasswordValid)
+            .bind(to: pwValid)
+            .disposed(by: disposeBag)
+    }
+    private func bindOutput() {
+        // output : red bullet button, login button
+        idValid.subscribe(onNext: { b in self.idValidView.isHidden = b })
+            .disposed(by: disposeBag)
+        pwValid.subscribe(onNext: { b in self.pwValidView.isHidden = b })
+            .disposed(by: disposeBag)
+        Observable.combineLatest(idValid, pwValid, resultSelector: { $0 && $1 })
+            .subscribe(onNext: { b in self.loginButton.isEnabled = b })
+            .disposed(by: disposeBag)
     }
 
     // MARK: - Logic
